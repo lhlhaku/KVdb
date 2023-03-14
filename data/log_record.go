57722a0e9839edbd484f579278c7d42d81f80144
haku <1826617030@qq.com>
2023-03-14 23:56:41 +0800

    添加数据文件的逻辑以及单元测试

--- data/log_record.go
+++ data/log_record.go
@@ -1,16 +1,6 @@
 package data
 
-type LogRecordPos struct {
-	//文件id,文件里面的偏移量
-	Fid    uint32
-	Offset int64
-}
-
-type LogRecord struct {
-	Key   []byte
-	Value []byte
-	Type  LogRecordType
-}
+import "encoding/binary"
 
 type LogRecordType = byte
 
@@ -19,8 +9,42 @@ const (
 	LogRecordDeleted
 )
 
+// crc type keySize valueSize
+// 4 +  1  +  5   +   5 = 15
+const maxLogRecordHeaderSize = binary.MaxVarintLen32*2 + 5
+
+// LogRecord 写入到数据文件的记录
+// 之所以叫日志，是因为数据文件中的数据是追加写入的，类似日志的格式
+type LogRecord struct {
+	Key   []byte
+	Value []byte
+	Type  LogRecordType
+}
+
+// LogRecord 的头部信息
+type logRecordHeader struct {
+	crc        uint32        // crc 校验值
+	recordType LogRecordType // 标识 LogRecord 的类型
+	keySize    uint32        // key 的长度
+	valueSize  uint32        // value 的长度
+}
+
+// LogRecordPos 数据内存索引，主要是描述数据在磁盘上的位置
+type LogRecordPos struct {
+	Fid    uint32 // 文件 id，表示将数据存储到了哪个文件当中
+	Offset int64  // 偏移，表示将数据存储到了数据文件中的哪个位置
+}
+
+// EncodeLogRecord 对 LogRecord 进行编码，返回字节数组及长度
 func EncodeLogRecord(logRecord *LogRecord) ([]byte, int64) {
-
 	return nil, 0
+}
 
+// 对字节数组中的 Header 信息进行解码
+func decodeLogRecordHeader(buf []byte) (*logRecordHeader, int64) {
+	return nil, 0
+}
+
+func getLogRecordCRC(lr *LogRecord, header []byte) uint32 {
+	return 0
 }
